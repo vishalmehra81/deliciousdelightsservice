8499574375de2fc5d446044624309ed6f9ff1c70
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-10-18 21:09:00 +0100

    Initial Commit

---
+++ src/main/java/com/codeclan/example/deliciousdelightsservice/DeliciousdelightsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.deliciousdelightsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciousdelightsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciousdelightsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/deliciousdelightsservice/DeliciousdelightsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.deliciousdelightsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousdelightsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
